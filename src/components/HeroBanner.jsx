60ab44261c2397fb820ed022963221156efaa794
Chaerul Umam <chaerulumamb244@gmail.com>
2023-03-08 23:12:00 +0700

    cart display

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import { urlFor } from "../../lib/client";
 
 const HeroBanner = ({ heroBanner }) => {
-  //   console.log(HeroBanner);
+  // console.log(HeroBanner);
   return (
     <div className="hero-banner-container">
       <div>
